80a01cdedd8bd04d41cf49313fe521195246e973
MattT253 <122591164+MattT253@users.noreply.github.com>
2025-01-20 17:49:35 +0000

    Coding complete for PE02

---
+++ PE02-MovieList/README.md
@@ -0,0 +1,10 @@
+# cs628-PE01
+
+Input:
+The inputs for this program is the App.js file, the Resume.js file, and the Resume.css file. The app.js file imports the Resume component and instantiates it. The Resume.js file contains all the simple components for the webpage and exports the Resume component. The Resume.css file defines the stylistic options for each of the components.
+
+Process:
+Using the create-react-app tool, it is possbile to quickly set up a single page web app for development. This process automatically sets up the webpage and loads file updates and displays them after refreshing the webpage.
+
+Output:
+The output is seen in the browser. It consists a green screen with the static information typical for a resume, formatted according to the Resume.css file.
---
+++ PE02-MovieList/movielist/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const Movies = [
+  {title: 'The Terminator', genres: ['Action', 'Sci-Fi'], releaseDate: '1984'},
+  {title: 'The Martian', genres: ['Sci-Fi'], releaseDate: '2015'},
+  {title: 'The Matrix', genres: ['Action', 'Sci-Fi'], releaseDate: '1999'},
+  {title: 'Tropic Thunder', genres: ['Action', 'Comedy'], releaseDate: '2008'},
+  {title: 'The Hangover', genres: ['Comedy'], releaseDate: '2009'}
+];
+
+function MovieList () {
+  const[genreValue, setGenreValue] = useState('All Genres')
+
+  const handleGenreSelect = (event) => {
+    setGenreValue(event.target.value);
+  }
+  
+  return (
+    <div class='container'>
+      <h1 class='header'>Movie List</h1>
+      <div class='dropdown'>
+        <select value={genreValue} onChange={handleGenreSelect}>
+          <option value="All Genres">All Genres</option>
+          <option value="Action">Action</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+          <option value="Comedy">Comedy</option>
+        </select>
+      </div>
+      <div>
+        <ul key={Movies.title}>{Movies.map((item) => (
+          <ul>
+            {genreValue === 'All Genres' || item.genres.includes(genreValue) ? (
+              <div class='movieCard' onClick={() => alert(`You clicked on "${item.title}"!`)}>
+                <p class='title'>{item.title}</p>
+                <p>Genres:</p>
+                {(item.genres).map((value) => <li class='genres'>{value}</li>)}
+                <p class='releaseDate'>Release date: {item.releaseDate}</p>
+              </div>) : null
+              }
+          </ul>
+          )
+        )}</ul>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <MovieList/>
+    </div>
+  );
+}
+
+export default App;
